7e08bafb203bdd708c1471de4ae4833e7fc2a021
Reginald Frank <regfran@amazon.com>
2020-06-29 20:59:47 -0500

    Added Login and Logout Servlets
    1) Why is the change being made?
    ---------------------------------
    Users will eventually be able to log in and log out of the website and store information about themselves.
    This change is the first step in that process.
    
    2) What has changed?
    ---------------------------------
    The Login and Logout Servlet classes have been created which will log the user in or out respectivly on GET request.
    
    3) Other concerns and/or dependent changes?
    ---------------------------------
    Currently UserService is being used to log in users, in the future we should change this to OAuth.

---
+++ src/main/java/com/google/sps/utils/Requests.java
@@ -0,0 +1,36 @@
+package com.google.sps.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Utility class to get information from HTTP Requests
+ */
+public class Requests {
+
+    /**
+     * Returns the value of a paramater from a HTTP request if present.
+     * If the request doesn't contain a value for the requested paramater, return a default value
+     */
+    public static String getParameter(HttpServletRequest request, String parameterName, String defaultValue) {
+        if (hasParameterValue(request, parameterName)) {
+            return request.getParameter(parameterName);
+        } else {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Returns true iff the request contains the parameter parameterName, its value is ignored
+     */
+    public static boolean hasParameter(HttpServletRequest request, String parameterName) {
+        String parameterValue = request.getParameter(parameterName);
+        return parameterValue == null;
+    }
+
+    /**
+     * Returns true iff the request contains the parameterName and it has a nonempty value
+     */
+    public static boolean hasParameterValue(HttpServletRequest request, String parameterName) {
+        return hasParameter(request, parameterName) && !"".equals(request.getParameter(parameterName));
+    }
+}
