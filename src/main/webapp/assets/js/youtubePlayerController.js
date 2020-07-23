674e70e2fa02bc1c2115426ad046d51a6a3c3fa4
axgtz <axguti@gmail.com>
2020-07-22 20:57:50 -0500

    Add YT Searchbar

--- src/main/webapp/assets/js/youtubePlayerController.js
+++ src/main/webapp/assets/js/youtubePlayerController.js
@@ -222,22 +222,3 @@ function seekTo(startTimeSec){
 function stopVideo(){
     player.stopVideo();
 }
-
-var state = 'stop';
-
-function buttonPlayPress() {
-    if(state==='stop'){
-      state='play';
-      var button = d3.select("#button_play").classed('btn-success', true); 
-      button.select("i").attr('class', "fa fa-pause");  
-    }
-    else if(state==='play' || state==='resume'){
-      state = 'pause';
-      d3.select("#button_play i").attr('class', "fa fa-play"); 
-    }
-    else if(state==='pause'){
-      state = 'resume';
-      d3.select("#button_play i").attr('class', "fa fa-pause");        
-    }
-    console.log("button play pressed, play was "+state);
-}
