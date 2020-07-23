05ca0bf39cdeddcd814df3cb5a51b96288714eda
Karol Gutierrez <kjgs.22@gmail.com>
2020-07-23 01:15:30 -0500

    Add search song servlet

---
+++ src/main/java/com/google/sps/servlets/SearchSongServlet.java
@@ -0,0 +1,63 @@
+package com.google.sps.servlets;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+@WebServlet("/search")
+public class SearchSongServlet extends HttpServlet {
+
+    /** Global instance properties filename. */
+    private static String PROPERTIES_FILENAME = "youtube.properties";
+
+    /** Global instance of the HTTP transport. */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    /** Global instance of the max number of videos we want returned (50 = upper limit per page). */
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+    /** Global instance of Youtube object to make all API requests. */
+    private static YouTube youtube;
+
+  // Returns comments in JSON format
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+    response.setContentType("application/json; charset=UTF-8;");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().println("[{}]");
+  }
+}
